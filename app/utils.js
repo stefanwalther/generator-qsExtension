18dc91c3a90212ac7976d0bbb53896298033ad96
Stefan Walther <swr@qlik.com>
2015-02-12 14:15:04 +0100

    formatting

--- app/utils.js
+++ app/utils.js
@@ -1,38 +1,39 @@
 /*global module, define, require*/
 'use strict';
-var async = require('async');
+var async = require( 'async' );
 var Winreg = require( 'winreg' );
 var path = require( 'path' );
-var Q = require('Q');
+var Q = require( 'Q' );
 
 module.exports = {
 
-    getExtensionPath: function() {
+	getExtensionPath: function () {
 
-        var deferred = Q.defer();
+		var deferred = Q.defer();
 
-        var regKey = new Winreg({
-            hive: Winreg.HKCU,
-            key: '\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders'
-        });
+		var regKey = new Winreg( {
+			hive: Winreg.HKCU,
+			key: '\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders'
+		} );
 
-        try {
-            regKey.values(function(err, items) {
-                for (var i in items) {
-                    if (items[i].name === 'Personal') {
-                        var dir = path.join(items[i].value, 'Qlik/Sense/Extensions' ).replace(/\\/g, "\\\\");
+		try {
+			regKey.values( function ( err, items ) {
+				for ( var i in items ) {
+					if ( items[i].name === 'Personal' ) {
+						var dir = path.join( items[i].value, 'Qlik/Sense/Extensions' ).replace( /\\/g, "\\\\" );
 
-                        dir = dir.replace('%USERPROFILE%', process.env['USERPROFILE' ].replace(/\\/g, "\\\\"));
+						dir = dir.replace( '%USERPROFILE%', process.env['USERPROFILE'].replace( /\\/g, "\\\\" ) );
 
-                        deferred.resolve( dir );
-                    }
-                }
-            });
-        }
-        catch (err) {
-            deferred.reject( err );
-        }
-        return deferred.promise;
+						deferred.resolve( dir );
+					}
+				}
+			} );
+		}
+		catch ( err ) {
+			deferred.reject( err );
+		}
+		return deferred.promise;
 
-    }
-};
+	}
+}
+;
