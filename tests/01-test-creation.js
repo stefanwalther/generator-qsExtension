4df9c4c4360c90f855c37f7316cb03aa5d1af559
Stefan Walther <swr@qlik.com>
2015-03-01 00:39:14 +0100

    get more on the enoent error for travis

--- tests/01-test-creation.js
+++ tests/01-test-creation.js
@@ -1,4 +1,21 @@
 /*global require, describe, beforeEach, it */
+
+// http://stackoverflow.com/questions/27688804/how-do-i-debug-error-spawn-enoent-on-node-js
+(function () {
+	'use strict';
+	var childProcess = require( "child_process" );
+	var oldSpawn = childProcess.spawn;
+
+	function mySpawn () {
+		console.log( 'spawn called' );
+		console.log( arguments );
+		var result = oldSpawn.apply( this, arguments );
+		return result;
+	}
+
+	childProcess.spawn = mySpawn;
+})();
+
 (function () {
 	'use strict';
 
