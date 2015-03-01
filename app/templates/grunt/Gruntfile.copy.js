803e348cabeff7290b3d82b2f7bebde7d0277509
Stefan Walther <swr@qlik.com>
2015-03-01 22:03:24 +0100

    lowercase projectconfig references

--- app/templates/grunt/Gruntfile.copy.js
+++ app/templates/grunt/Gruntfile.copy.js
@@ -1,25 +1,25 @@
 /*global module*/
 module.exports = function ( grunt ) {
-    'use strict';
+	'use strict';
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    return {
-        options: {
-          processContentExclude: ['**/*.{png,gif,jpg,ico,psd}']
-        },
-        copy_to_dist: {
-            expand: true,                   // allow dynamic building
-            cwd: '../src/',                 // change base dir
-            //src: ['**', '!docs/**'],      // source files mask
-            src: ['**'],
-            dest: '../dist/',               // destination folder
-            flatten: false                  // remove all unnecessary nesting
-        },
-        copy_to_desktop: {
-            expand: true,
-            cwd: '../dist/',
-            src: '**',
-            dest: '<%= projectconfig.general.LocalExtensionPath%>/<%= projectConfig.general.ExtensionNamespace%><%= projectconfig.general.ExtensionNameSafe%>/'
-        }
-    };
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	return {
+		options: {
+			processContentExclude: ['**/*.{png,gif,jpg,ico,psd}']
+		},
+		copy_to_dist: {
+			expand: true,                   // allow dynamic building
+			cwd: '../src/',                 // change base dir
+			//src: ['**', '!docs/**'],      // source files mask
+			src: ['**'],
+			dest: '../dist/',               // destination folder
+			flatten: false                  // remove all unnecessary nesting
+		},
+		copy_to_desktop: {
+			expand: true,
+			cwd: '../dist/',
+			src: '**',
+			dest: '<%= projectconfig.general.LocalExtensionPath%>/<%= projectconfig.general.ExtensionNamespace%><%= projectconfig.general.ExtensionNameSafe%>/'
+		}
+	};
 };
