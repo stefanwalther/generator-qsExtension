8510bf6ae013bdd1a2ab264d4cb7bb2498f19b04
Stefan Walther <swr@qlik.com>
2015-03-01 20:57:50 +0100

    Fix: getExtensionPath on non windows os

--- app/utils.js
+++ app/utils.js
@@ -11,26 +11,31 @@ module.exports = {
 
 		var deferred = Q.defer();
 
-		var regKey = new Winreg( {
-			hive: Winreg.HKCU,
-			key: '\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders'
-		} );
-
-		try {
-			regKey.values( function ( err, items ) {
-				for ( var i in items ) {
-					if ( items[i].name === 'Personal' ) {
-						var dir = path.join( items[i].value, 'Qlik/Sense/Extensions' ).replace( /\\/g, "\\\\" );
-
-						dir = dir.replace( '%USERPROFILE%', process.env['USERPROFILE'].replace( /\\/g, "\\\\" ) );
-
-						deferred.resolve( dir );
-					}
-				}
+		if ( process.platform === 'win32' ) {
+			var regKey = new Winreg( {
+				hive: Winreg.HKCU,
+				key: '\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders'
 			} );
+
+			try {
+				regKey.values( function ( err, items ) {
+					for ( var i in items ) {
+						if ( items[i].name === 'Personal' ) {
+							var dir = path.join( items[i].value, 'Qlik/Sense/Extensions' ).replace( /\\/g, "\\\\" );
+
+							dir = dir.replace( '%USERPROFILE%', process.env['USERPROFILE'].replace( /\\/g, "\\\\" ) );
+
+							deferred.resolve( dir );
+						}
+					}
+				} );
+			}
+			catch ( err ) {
+				deferred.reject( err );
+			}
 		}
-		catch ( err ) {
-			deferred.reject( err );
+		else {
+			deferred.resolve( path.homedir() + '\\Documents\\Qlik\\Sense\\Extensions' );
 		}
 		return deferred.promise;
 
