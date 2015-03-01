58bdc105b9ed3f5627357d5adfba47438497f7f1
Stefan Walther <swr@qlik.com>
2015-03-01 20:46:53 +0100

    re-activate spawn trace

--- tests/01-test-creation.js
+++ tests/01-test-creation.js
@@ -3,17 +3,17 @@
 // http://stackoverflow.com/questions/27688804/how-do-i-debug-error-spawn-enoent-on-node-js
 (function () {
 	'use strict';
-	//var childProcess = require( "child_process" );
-	//var oldSpawn = childProcess.spawn;
-	//
-	//function mySpawn () {
-	//	console.log( 'spawn called' );
-	//	console.log( arguments );
-	//	var result = oldSpawn.apply( this, arguments );
-	//	return result;
-	//}
-	//
-	//childProcess.spawn = mySpawn;
+	var childProcess = require( "child_process" );
+	var oldSpawn = childProcess.spawn;
+
+	function mySpawn () {
+		console.info( 'spawn called' );
+		console.trace( arguments );
+		var result = oldSpawn.apply( this, arguments );
+		return result;
+	}
+
+	childProcess.spawn = mySpawn;
 })();
 
 (function () {
