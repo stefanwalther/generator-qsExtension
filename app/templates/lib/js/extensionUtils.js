c25cba07f03a4565df7ce1608cf6ee97d4c4e517
Stefan Walther <swr@qlik.com>
2015-03-04 22:12:37 +0100

    Added save log, needs some testing

--- app/templates/lib/js/extensionUtils.js
+++ app/templates/lib/js/extensionUtils.js
@@ -4,6 +4,42 @@ define( [
 ], function ( $ /*, _*/ ) {
 	'use strict';
 
+	// Taken from http://www.briangrinstead.com/blog/console-log-helper-function
+	// Full version of `log` that:
+	//  * Prevents errors on console methods when no console present.
+	//  * Exposes a global 'log' function that preserves line numbering and formatting.
+	(function () {
+		var method;
+		var noop = function () { };
+		var methods = [
+			'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+			'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+			'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+			'timeStamp', 'trace', 'warn'
+		];
+		var length = methods.length;
+		var console = (window.console = window.console || {});
+
+		while (length--) {
+			method = methods[length];
+
+			// Only stub undefined methods.
+			if (!console[method]) {
+				console[method] = noop;
+			}
+		}
+
+
+		if (Function.prototype.bind) {
+			window.log = Function.prototype.bind.call(console.log, console);
+		}
+		else {
+			window.log = function() {
+				Function.prototype.apply.call(console.log, console, arguments);
+			};
+		}
+	})();
+
 	return {
 
 		/**
@@ -13,6 +49,8 @@ define( [
 		addStyleToHeader: function ( cssContent ) {
 			$( "<style>" ).html( cssContent ).appendTo( "head" );
 		}
+
+
 
 	};
 
