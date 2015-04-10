2f1388dcbe83f1bb435f53c9edf47b14fe3e04fe
Stefan Walther <swr@qlik.com>
2015-04-10 22:49:24 +0200

    Improvements to addStyleToHeader

--- app/templates/lib/js/extensionUtils.js
+++ app/templates/lib/js/extensionUtils.js
@@ -43,16 +43,25 @@ define( [
 
 		/**
 		 * Add a style to the document's header.
-		 * @param cssContent (String)
+		 * @param cssContent {String} CSS content to be added to the header
+		 * @param id {String} If id is passed, addStyleToHeader will check if there has already been added a style with the given id, if yes, the css content will not be added to the header again
 		 */
-		addStyleToHeader: function ( cssContent ) {
-			$( "<style>" ).html( cssContent ).appendTo( "head" );
+		addStyleToHeader: function ( cssContent, id ) {
+			if ( id && typeof id === 'string' ) {
+				if ( !$( '#id' ).length ) {
+					$( "<style>" )
+						.attr( 'id', id )
+						.html( cssContent ).appendTo( "head" );
+				}
+			} else {
+				$( "<style>" ).html( cssContent ).appendTo( "head" );
+			}
 		},
 
 		/**
 		 * Add as style link to the document's header
-		 * @param linkUrl Url to the CSS file
-		 * @param id If an id is passed, the function will check if this style link has already been added or not.
+		 * @param {String} linkUrl Url to the CSS file
+		 * @param {String}  id If an id is passed, the function will check if this style link has already been added or not.
 		 * If yes, it will not be added again.
 		 */
 		addStyleLinkToHeader: function ( linkUrl, id ) {
